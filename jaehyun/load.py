8fdd07d563f62c3819833fd8f35fdbd562e7349f
Jaehyun-Jeong <wogus0948@naver.com>
2022-10-05 09:30:07 +0000

    test

--- jaehyun/load.py
+++ jaehyun/load.py
@@ -1,6 +1,34 @@
+import torch
+import torchvision
+import torchvision.transforms as transforms
 
 
-class Loader():
+def Load(
+        directory: str):
 
-    def __init__(self):
-        pass
+    transform = transforms.Compose(
+        [transforms.ToTensor(),
+         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+    batch_size = 4
+
+    trainset = torchvision.datasets.ImageFolder(
+            root=directory,
+            transform=transform)
+    trainloader = torch.utils.data.DataLoader(
+            trainset,
+            batch_size=batch_size,
+            shuffle=True,
+            num_workers=2)
+
+    testset = torchvision.datasets.ImageFolder(
+            root=directory,
+            download=True,
+            transform=transform)
+    testloader = torch.utils.data.DataLoader(
+            testset,
+            batch_size=batch_size,
+            shuffle=False,
+            num_workers=2)
+
+    return trainloader, testloader
