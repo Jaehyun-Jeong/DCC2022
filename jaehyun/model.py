f383671f37e47e472e3a59a7c6698fb3e4a59a6c
Jaehyun-Jeong <wogus0948@naver.com>
2022-10-22 15:59:42 +0000

    fixed to test train

--- jaehyun/model.py
+++ jaehyun/model.py
@@ -23,23 +23,69 @@ class Model(nn.Module):
         return x
 
 
+class VGG16(nn.Module):
+
+    def __init__(
+            self,
+            inputs: tuple = (3, 224, 224),
+            outputs: int = 20):
+
+        super().__init__()
+
+        self.feature = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(64, 64, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2, padding=0, dilation=1, ceil_mode=False),
+            nn.Conv2d(64, 128, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(128, 128, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2, padding=0, dilation=1, ceil_mode=False),
+            nn.Conv2d(128, 256, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2, padding=0, dilation=1, ceil_mode=False),
+            nn.Conv2d(256, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2, padding=0, dilation=1, ceil_mode=False),
+            nn.Conv2d(512, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2, padding=0, dilation=1, ceil_mode=False),
+        )
+
+        self.avgpool = nn.AdaptiveAvgPool2d(output_size=(7, 7))
+
+        self.classifier = nn.Sequential(
+            nn.Linear(in_features=25088, out_features=4096, bias=True),
+            nn.ReLU(inplace=True),
+            nn.Dropout(p=0.5, inplace=False),
+            nn.Linear(in_features=4096, out_features=4096, bias=True),
+            nn.ReLU(inplace=True),
+            nn.Dropout(p=0.5, inplace=False),
+            nn.Linear(in_features=4096, out_features=outputs, bias=True),
+        )
+
+    # Input as 224x224x3 image
+    def forward(self, x):
+        x = self.feature(x)
+        x = self.avgpool(x)
+        x = self.classifier(x)
+
+        return x
+
 if __name__ == "__main__":
-    import torchvision
-
-    # VGG16
-    model = torchvision.models.vgg16()
-    print("VGG16")
+    model = VGG16()
     print(model)
-    print(400*"=")
-
-    # googlenet
-    model = torch.hub.load('pytorch/vision:v0.10.0', 'googlenet')
-    print("GoogleNet")
-    print(model)
-    print(400*"=")
-
-    # resnet-18
-    model = torch.hub.load('pytorch/vision:v0.10.0', 'resnet18')
-    print("Resnet18")
-    print(model)
-    print(400*"=")
--- jaehyun/train.py
+++ jaehyun/train.py
@@ -1,42 +1,115 @@
 from model import Model
 from load import Load
 
+import torch
 import torch.optim as optim
 from torch import nn
+import torchvision.transforms as transforms
 
 # Ignore warnings
 import warnings
 warnings.filterwarnings("ignore", category=UserWarning)
 
-net = Model()
-Loader = Load()
-data_set, data_loader = Loader.tensor("./dataset")
 
-numpy_data = Loader.numpy("./dataset")
+class Trainer():
 
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+    def __init__(
+            self,
+            model: nn.Module,
+            optimizer,
+            criterion,
+            device: torch.device = torch.device('cpu'),
+            ):
 
-for epoch in range(2):  # loop over the dataset multiple times
+        self.device = device
+        self.model = model.to(self.device)
+        self.optimizer = optimizer
+        self.criterion = criterion
 
-    running_loss = 0.0
-    for i, data in enumerate(train_loader, 0):
-        # get the inputs; data is a list of [inputs, labels]
-        inputs, labels = data
+    def train(
+            self,
+            epochs: int,
+            train_loader,
+            test_loader):
 
-        # zero the parameter gradients
-        optimizer.zero_grad()
+        for epoch in range(epochs):  # loop over the dataset multiple times
+            for i, data in enumerate(train_loader):
+                # get the inputs; data is a list of [inputs, labels]
+                inputs = data[0].to(self.device)
+                labels = data[1].to(self.device)
 
-        # forward + backward + optimize
-        outputs = net(inputs)
-        loss = criterion(outputs, labels)
-        loss.backward()
-        optimizer.step()
+                # zero the parameter gradients
+                self.optimizer.zero_grad()
 
-        # print statistics
-        running_loss += loss.item()
-        if i % 2000 == 1999:    # print every 2000 mini-batches
-            print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}')
-            running_loss = 0.0
+                # forward + backward + optimize
+                outputs = self.model(inputs)
+                loss = self.criterion(outputs, labels)
+                loss.backward()
+                self.optimizer.step()
 
-print('Finished Training')
+            accuracy = self.test(test_loader)
+            print(f'{epoch + 1}: Accuracy {accuracy} %')
+
+    @torch.no_grad()
+    def test(
+            self,
+            test_loader):
+
+        correct = 0
+        total = 0
+
+        for data in test_loader:
+            images = data[0].to(self.device)
+            labels = data[1].to(self.device)
+
+            outputs = self.model(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+        return 100 * correct // total
+
+    def f1_score(self):
+        pass
+
+
+if __name__ == "__main__":
+    # Use Gpu
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+    model = Model()
+
+    # Train loader
+    train_transformer = [
+        transforms.ToTensor(),
+        transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
+        transforms.Resize((32, 32))]
+    train_dataloader = Load(
+            train_transformer,
+            batch_size=128)
+    _, trainloader = train_dataloader("./final_dataset/train")
+
+    # Test loader
+    test_transformer = [
+        transforms.ToTensor(),
+        transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
+        transforms.Resize((32, 32))]
+    test_dataloader = Load(
+            test_transformer,
+            batch_size=128)
+    _, testloader = test_dataloader("./final_dataset/test")
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+
+    # Init Trainer
+    trainer = Trainer(
+            model=model,
+            optimizer=optimizer,
+            criterion=criterion,
+            device=device)
+
+    # Train
+    trainer.train(
+            3,
+            trainloader,
+            testloader)
