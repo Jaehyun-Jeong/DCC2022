1e1347991afc01e49bf4a1b0490dd603c1f703b3
Jaehyun-Jeong <wogus0948@naver.com>
2022-10-28 01:44:26 +0000

    created for learning rate scheduling

--- jaehyun/VGG16/test_04.py
+++ jaehyun/VGG16/test_04.py
@@ -33,13 +33,20 @@ valid_dataloader = Load(
 _, valloader = valid_dataloader("../augmented_dataset/valid")
 
 criterion = nn.CrossEntropyLoss()
-optimizer = optim.Adam(model.parameters(), lr=1e-6)
+optimizer = optim.Adam(model.parameters(), lr=0.03)
+exp_lr_scheduler = lr_scheduler.StepLR(
+        optimizer,
+        step_size=30,
+        gamma=0.1)
+
+exp_lr_scheduler.last_epoch = 30 * 4 # only 4 times update
 
 # Init Trainer
 trainer = Trainer(
         model=model,
         optimizer=optimizer,
         criterion=criterion,
+        scheduler=exp_lr_scheduler,
         device=device,
         )
 
